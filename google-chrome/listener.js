a6f25b753c6385002064a5064544f69fbb9d5a9a
Brad Lambeth <brad@lambeth.us>
2011-05-31 08:25:43 -0500

    change javascript functions for Play, Pause, Prev, and Next Song

--- google-chrome/listener.js
+++ google-chrome/listener.js
@@ -1,33 +1,63 @@
 var keygmusicListener = {
 
-  CloudPlayer: function(toggle)
-  {
-    // Chrome ... WTF did you made me write?
-    var elem = document.createElement("script");
-    elem.type = "text/javascript";
-    elem.innerHTML = toggle;
+  // Function adapted from http://code.google.com/p/music-beta-controller/
+  // Find a the first tab in the current window that is showing Music Beta
+  // and call callback with the id of that tab.
+  FindMusicBetaTab: function(callback) {
+    chrome.windows.getAll({populate: true}, function(windows) {
+        var pattern = 'https?\:\/\/music\.google\.com\/music\/listen.*';
+        for (var window = 0; window < windows.length; window++) {
+          for (var i = 0; i < windows[window].tabs.length; i++) {
+            if (windows[window].tabs[i].url.match(pattern)) {
+              callback(windows[window].tabs[i].id)
+              return;
+            }
+          }
+        }
+        callback(null);
+      });
+  }
 
-    var append = document.head.appendChild(elem);
-    document.head.removeChild(append);
-  },
+  // Function adapted from http://code.google.com/p/music-beta-controller/
+  // Send the given command to a tab showing Music Beta,
+  // or open one if non exists.
+  sendCommand: function(command) {
+    keygmusicListener.FindMusicBetaTab(function(tab_id) {
+        if (tab_id) {
+          if (command == "foreground") {
+            chrome.tabs.update(tab_id, {selected: true});
+          } else {
+            chrome.tabs.executeScript(tab_id,
+                {
+                  code: "location.assign('javascript:SJBpost(\"" + command +
+                        "\");void 0');",
+                  allFrames: true
+                });
+          }
+        } else {
+          chrome.tabs.create({url: 'http://music.google.com/music/listen',
+                              selected: true});
+        }
+      });
+  }
 
   // Init keygmusicListener object
   init: function(){
 
-    if (window.location.href.search(/^https\:\/\/www\.amazon\.com\/gp\/dmusic\/mp3\/player/) != -1){
+    if (window.location.href.search(/^http\:\/\/music\.google\.com\/music\/listen/) != -1){
 
       chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         var allToggles = {
-          "play"      : function(){ keygmusicListener.CloudPlayer("window.amznMusic.widgets.player.masterPlay();"); },
-          "stop"      : function(){ keygmusicListener.CloudPlayer("window.amznMusic.widgets.player.pause();"); },
-          "prev"      : function(){ keygmusicListener.CloudPlayer("window.amznMusic.widgets.player.playHash('previous', null, null);"); },
-          "next"      : function(){ keygmusicListener.CloudPlayer("window.amznMusic.widgets.player.playHash('next', null, null);"); },
+          "play"      : function(){ keygmusicListener.sendCommand("playPause"); },
+          "stop"      : function(){ keygmusicListener.sendCommand("stop"); },
+          "prev"      : function(){ keygmusicListener.sendCommand("prevSong"); },
+          "next"      : function(){ keygmusicListener.sendCommand("nextSong"); },
 
-          "volup"     : function(){ keygmusicListener.CloudPlayer("var volCont=window.jQuery('.volumeContainer');var volNow=volCont.slider('option','value');if(volNow<=90){window.amznMusic.playerInterface.setVolume((volNow/100)+0.1);volCont.slider('option','value',volNow+10)}else{window.amznMusic.playerInterface.setVolume(1);volCont.slider('option','value',100)}") },
-          "voldown"   : function(){ keygmusicListener.CloudPlayer("var volCont=window.jQuery('.volumeContainer');var volNow=volCont.slider('option','value');if(volNow>=10){window.amznMusic.playerInterface.setVolume((volNow/100)-0.1);volCont.slider('option','value',volNow-10)}else{window.amznMusic.playerInterface.setVolume(0);volCont.slider('option','value',0)}"); },
+//          "volup"     : function(){ keygmusicListener.sendCommand("var volCont=window.jQuery('.volumeContainer');var volNow=volCont.slider('option','value');if(volNow<=90){window.amznMusic.playerInterface.setVolume((volNow/100)+0.1);volCont.slider('option','value',volNow+10)}else{window.amznMusic.playerInterface.setVolume(1);volCont.slider('option','value',100)}") },
+//          "voldown"   : function(){ keygmusicListener.sendCommand("var volCont=window.jQuery('.volumeContainer');var volNow=volCont.slider('option','value');if(volNow>=10){window.amznMusic.playerInterface.setVolume((volNow/100)-0.1);volCont.slider('option','value',volNow-10)}else{window.amznMusic.playerInterface.setVolume(0);volCont.slider('option','value',0)}"); },
         };
 
-        if (request.method == "CloudPlayer" && allToggles[request.action] != undefined){
+        if (request.method == "sendCommand" && allToggles[request.action] != undefined){
 
           try{
             allToggles[request.action]();
